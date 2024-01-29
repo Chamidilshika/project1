a7a5e0b7be2a79d487bc90e5b49f0f91e4f6c612
Chamidilshika <158010483+Chamidilshika@users.noreply.github.com>
2024-01-29 09:49:42 +0530

    Add files via upload
    ok

---
+++ jframeproject/JFrameProject.java
@@ -0,0 +1,16 @@
+
+package jframeproject;
+
+public class JFrameProject {
+
+    public static void main(String[] args) {
+       
+        FirstForm f = new FirstForm();
+        
+        f.setSize(400, 200);
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
+    }
+    
+    
+}
